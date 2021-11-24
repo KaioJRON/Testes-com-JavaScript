30589fdba2a68504fe0b3036dc58a6ab9e0762eb
Kaio Junior Oliveski Novak <93236804+KaioJRON@users.noreply.github.com>
2021-11-24 14:15:00 -0300

    Update atividade 4.js

--- atividade 4.js
+++ atividade 4.js
@@ -1,3 +1,3 @@
-var numero1 = prompt('digite um numero');
-var numero2 = prompt('digite um numero');
+var numero1 = prompt('primero numero');
+var numero2 = prompt('segundo numero');
 document.getElementById('texto').innerHTML = (parseInt(numero1)+parseInt(numero2))
